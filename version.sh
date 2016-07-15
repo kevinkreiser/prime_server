f12782537a09da9ec1f49259d8e058eacc717bce
Anthony J. Bentley <anthony@anjbe.name>
2016-07-15 01:07:42 -0600

    Instead of non-portable grep -m1 | sed, use grep | sed 1q.

--- version.sh
+++ version.sh
@@ -8,9 +8,9 @@ if [ ! -f prime_server/prime_server.hpp ]; then
 	exit 1
 fi
 
-major=$(grep -m1 -F VERSION_MAJOR prime_server/prime_server.hpp | sed -e "s/.*MAJOR \([0-9][0-9]*\)/\1/g")
-minor=$(grep -m1 -F VERSION_MINOR prime_server/prime_server.hpp | sed -e "s/.*MINOR \([0-9][0-9]*\)/\1/g")
-patch=$(grep -m1 -F VERSION_PATCH prime_server/prime_server.hpp | sed -e "s/.*PATCH \([0-9][0-9]*\)/\1/g")
+major=$(grep -F VERSION_MAJOR prime_server/prime_server.hpp | sed -e "s/.*MAJOR \([0-9][0-9]*\)/\1/g" -e 1q)
+minor=$(grep -F VERSION_MINOR prime_server/prime_server.hpp | sed -e "s/.*MINOR \([0-9][0-9]*\)/\1/g" -e 1q)
+patch=$(grep -F VERSION_PATCH prime_server/prime_server.hpp | sed -e "s/.*PATCH \([0-9][0-9]*\)/\1/g" -e 1q)
 
 if [ -z $major ] || [ -z $minor ] || [ -z $patch ]; then
 	echo "Malformed version information"
