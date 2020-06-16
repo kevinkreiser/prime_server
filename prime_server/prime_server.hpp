f0d0a9333201dea4a394fc3e6c57f90973f33188
Kevin Kreiser <kevinkreiser@gmail.com>
2020-06-16 09:40:31 -0400

    prepare for release 0.6.6

--- prime_server/prime_server.hpp
+++ prime_server/prime_server.hpp
@@ -4,7 +4,7 @@
 //some version info
 #define PRIME_SERVER_VERSION_MAJOR 0
 #define PRIME_SERVER_VERSION_MINOR 6
-#define PRIME_SERVER_VERSION_PATCH 5
+#define PRIME_SERVER_VERSION_PATCH 6
 
 #include <functional>
 #include <string>
