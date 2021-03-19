aa06d3b8ceb30f554827464a2a612e993ffe096c
Kevin Kreiser <kevinkreiser@gmail.com>
2021-03-18 22:40:33 -0400

    fixes #65 and fixes #73

--- test/interrupt.cpp
+++ test/interrupt.cpp
@@ -44,6 +44,11 @@ protected:
   }
 };
 
+worker_t::result_t
+busy_work(const std::list<zmq::message_t>&, void*, const worker_t::interrupt_function_t&) {
+  while (true) {}
+}
+
 void test_early() {
   zmq::context_t context;
 
@@ -62,15 +67,10 @@ void test_early() {
   proxy.detach();
 
   // busy worker
-  std::thread worker(
-      std::bind(&testable_worker_t::work,
-                testable_worker_t(context, "ipc:///tmp/test_early_proxy_downstream",
-                                  "ipc:///dev/null", "ipc:///tmp/test_early_results",
-                                  "ipc:///tmp/test_early_interrupt",
-                                  [](const std::list<zmq::message_t>&, void*,
-                                     const worker_t::interrupt_function_t&) -> worker_t::result_t {
-                                    while (true) {}
-                                  })));
+  std::thread worker(std::bind(&testable_worker_t::work,
+                               testable_worker_t(context, "ipc:///tmp/test_early_proxy_downstream",
+                                                 "ipc:///dev/null", "ipc:///tmp/test_early_results",
+                                                 "ipc:///tmp/test_early_interrupt", busy_work)));
   worker.detach();
 
   // we want a client that is very fickle, we need the client to send a request and then bail right
@@ -172,13 +172,9 @@ void test_timeout() {
 
   // busy worker
   std::thread worker(
-      std::bind(&worker_t::work,
-                worker_t(context, "ipc:///tmp/test_timeout_proxy_downstream", "ipc:///dev/null",
-                         "ipc:///tmp/test_timeout_results", "ipc:///tmp/test_timeout_interrupt",
-                         [](const std::list<zmq::message_t>&, void*,
-                            const worker_t::interrupt_function_t&) -> worker_t::result_t {
-                           while (true) {}
-                         })));
+      std::bind(&worker_t::work, worker_t(context, "ipc:///tmp/test_timeout_proxy_downstream",
+                                          "ipc:///dev/null", "ipc:///tmp/test_timeout_results",
+                                          "ipc:///tmp/test_timeout_interrupt", busy_work)));
   worker.detach();
 
   std::string request = netstring_entity_t::to_string("wart uf mi");
