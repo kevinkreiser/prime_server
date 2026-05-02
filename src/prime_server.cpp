52cdb54f25e803ea90240ebb806237befaebc151
Kevin Kreiser <kevinkreiser@gmail.com>
2026-05-01 23:57:00 -0400

    more windows compat

--- src/prime_server.cpp
+++ src/prime_server.cpp
@@ -5,8 +5,6 @@
 #include <unordered_set>
 
 #ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
 #include <windows.h>
 #include <chrono>
 #else
--- test/shutdown.cpp
+++ test/shutdown.cpp
@@ -9,8 +9,6 @@
 #include <thread>
 
 #ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
 #include <windows.h>
 #else
 #include <sys/wait.h>
